8a4d55659dfe3b31d56e58eda7bd1ad45d373891
deshiyan1010 <vinayakamikkal@gmail.com>
2022-04-23 17:54:11 +0530

    hashq init

---
+++ hashQ.py
@@ -0,0 +1,66 @@
+
+from collections import deque
+import pickle
+import os
+import time
+
+
+class Node:
+    def __init__(self,data) -> None:
+        self.timestamp = time.time()
+        self.data = data
+
+
+
+
+class Queue:
+    
+    def __init__(self,keep=100):
+        self.arr = deque()
+        self.keep = keep
+        self.load()
+        self.locator = {}
+
+    def load(self,file="heap"):
+        if file not in set(os.listdir()):
+            return
+        pick = pickle.load(open(file, 'rb'))
+        self.arr = pick.arr
+        self.keep = pick.keep
+
+
+    def save(self,file="heap"):
+        pickle.dump(self,open(file, 'wb'))
+
+
+    def insert(self,val):
+        node = Node(val)
+        self.locator[val] = node
+        self.arr.append(node)
+        
+        if len(self.arr)>self.keep:
+            popped = self.pop()
+            self.locator.pop(popped.data)
+
+    def search(self,data):
+        if data in self.locator:
+            return True
+        return False
+
+    def pop(self):
+        return self.arr.popleft() 
+    
+
+
+
+if __name__=="__main__":
+    h = Queue(5)
+    h.insert(10)
+    h.insert(30)
+    h.insert(49)
+
+    o = h.pop()
+    print(o.timestamp,o.data)
+    o = h.pop()
+    print(o.timestamp,o.data)
+    
