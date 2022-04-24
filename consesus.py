4e05604b56d8a33340b5804f072b9c252b851623
deshiyan1010 <vinayakamikkal@gmail.com>
2022-04-24 18:53:46 +0530

    block to trie added

--- consesus.py
+++ consesus.py
@@ -11,10 +11,12 @@ class Consesus:
         previous_proof = self.blockchain.get_previous_block()['proof']
         hash_operation = hashlib.sha256(
             str(new_proof**2 - previous_proof**2).encode()).hexdigest()
-        if hash_operation[:4] == '0000':
+        if hash_operation[:4] == '0000' and block['transactions'][-1]['sender']=="COINBASE" and block['transactions'][-1]['amount']==1:
             return True
         return False
 
 
     def add_block(self,block):
+        for txn in block['transactions']:
+            self.blockchain.trie.insert_txn(txn)
         self.blockchain.append_block(block)
